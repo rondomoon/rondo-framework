de174b8894863a2210a99162b153ac6041f70886
Jerko Steiner <jerko.steiner@gmail.com>
2019-01-18 20:24:19 +0100

    Create package common

---
+++ packages/common/src/index.ts
@@ -0,0 +1,21 @@
+export type IMethod = 'get'
+ | 'post'
+ | 'put'
+ | 'delete'
+ | 'patch'
+ | 'head'
+ | 'options'
+
+export interface IRoutes {
+  // has to be any because otherwise TypeScript will start
+  // throwing error and interfaces without an index signature
+  // would not be usable
+  [route: string]: any
+}
+
+export interface IRoute {
+  params: any
+  query: any
+  body: any
+  response: any
+}
